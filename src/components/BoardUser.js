055b1dc228202abf96843a4ad3b90f292a173d86
Jorge Betancur <jabetancur12@gmail.com>
2022-07-30 14:23:37 +0000

    feat(board): max min values added

--- src/components/BoardUser.js
+++ src/components/BoardUser.js
@@ -3,23 +3,34 @@ import Select from './Select/Select'
 import useSWR from 'swr'
 import Calendar from './Calendar/Calendar'
 import Chart from './Chart/Chart'
+import AuthService from '../services/auth.service'
+import axios from 'axios'
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+const fetcher = (url, token) =>
+  axios
+    .get(url, { headers: { Authorization: 'Bearer ' + token } })
+    .then((res) => res.data)
 
 const dt = new Date()
 dt.setHours(dt.getHours() - 24)
 
 const BoardUser = () => {
+  const { token } = AuthService.getCurrentUser()
   const [startDate, setStartDate] = useState(dt)
   const [endDate, setEndDate] = useState(new Date())
   const [value, setValue] = useState({ value: 'minute' })
   const [period, setPeriod] = useState(15)
 
-  const url = `http://192.168.0.6:5000/api/sensor?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&unit=${
+  const url = `http://192.168.0.6:5000/api/sensor/data?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}&unit=${
     value.value
   }&period=${period}`
 
-  const { data } = useSWR(url, fetcher)
+  const { data } = useSWR([url, token], fetcher)
+
+  const maxT = data && Math.max(...data.map((o) => o.averageT))
+  const minT = data && Math.min(...data.map((o) => o.averageT))
+  const maxH = data && Math.max(...data.map((o) => o.averageH))
+  const minH = data && Math.min(...data.map((o) => o.averageH))
 
   const getUnitValue = (value) => {
     setValue(value)
@@ -47,6 +58,12 @@ const BoardUser = () => {
           </div>
           <div className="card">
             <Select getValue={getUnitValue} />
+          </div>
+          <div className="card">
+            <p>maxT: {maxT && maxT.toFixed(2)} C°</p>
+            <p>minT: {minT && minT.toFixed(2)} C°</p>
+            <p>maxH: {maxH && maxH.toFixed(2)} %</p>
+            <p>minH: {minH && minH.toFixed(2)} %</p>
           </div>
         </div>
       </div>
